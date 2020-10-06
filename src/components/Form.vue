d9b4bda97301657260b119162893a477b331235e
Pitchapa-Jeekhumwon <pitchapa.jeekhumwon@gmail.com>
2020-10-06 14:56:13 +0700

    commit

---
+++ src/components/Form.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container fluid">
+    <br />
+    <div>
+      <div class="card">
+        <div class="card-header">
+          <h2>Register</h2>
+        </div>
+        <div class="card-body">
+          <form>
+            <div class="row">
+            <div class="form-group col-md-6">
+              <label>Name</label>
+              <input type="text" class="form-control"/>
+            </div>
+            <div class="form-group col-md-6">
+              <label>Last name</label>
+              <input type="text" class="form-control"/>
+            </div>
+            
+            <div class="form-group col-md-6">
+              <label>Date of brith</label>
+              <input type="date" class="form-control"/>
+            </div>
+            <div class="form-group col-md-6">
+              <label>Gender</label>
+              <input type="text" class="form-control"/>
+            </div>
+            <div class="form-group col-md-6">
+              <label>E-Mail</label>
+              <input type="text" class="form-control"/>
+            </div>
+            <div class="form-group col-md-6">
+              <label>Password</label>
+              <input type="text" class="form-control"/>
+            </div>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+            
+          </form>          
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+ <script>
+ </script>
+
+ <style></style>
